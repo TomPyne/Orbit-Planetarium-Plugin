648491b8d8eafbea64a45032e3dcd2f3cc85b277
Tom Pyne <tompyne@live.co.uk>
2018-04-13 10:37:01 +0100

    Created the surface feature class

---
+++ Plugins/OrbitPlanetarium/Source/OrbitPlanetarium/Private/OP_SurfaceFeature.cpp
@@ -0,0 +1,7 @@
+// Tom Pyne - 2018
+
+#include "OP_SurfaceFeature.h"
+
+
+
+
---
+++ Plugins/OrbitPlanetarium/Source/OrbitPlanetarium/Public/OP_SurfaceFeature.h
@@ -0,0 +1,26 @@
+// Tom Pyne - 2018
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataAsset.h"
+#include "OP_SurfaceFeature.generated.h"
+
+/**
+ * 
+ */
+UCLASS(EditInlineNew)
+class ORBITPLANETARIUM_API UOP_SurfaceFeature : public UDataAsset
+{
+	GENERATED_BODY()
+	
+public:
+
+	// The Heightmap decal
+	UPROPERTY(EditAnywhere)
+	class UTexture2D* HeightMap;
+	
+	// The maximum amount that can appear on a surface
+	UPROPERTY(EditAnywhere)
+	int MaxAmount = 10;	
+};
